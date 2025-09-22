bf78fe49f54da13b6988332e39b3d0cd32d90038
hsuanice <hsuanice@gmail.com>
2025-09-22 17:51:36 +0800

    Update hsuanice_RGWH Core ExtState Set.lua

--- Beta Testing/hsuanice_RGWH Core ExtState Set.lua
+++ Beta Testing/hsuanice_RGWH Core ExtState Set.lua
@@ -1,6 +1,6 @@
 --[[
 @description RGWH Core ExtState Set (project-scope)
-@version 250921_1512 add new glue fx extstate
+@version 250922_1750 update renamed functino glue and edge cues
 @author hsuanice
 @about
   快速設定 RGWH Core 用到的 ExtState（寫在當前專案）。
@@ -28,8 +28,8 @@ local CFG = {
   RENDER_APPLY_MODE  = "mono",   -- "mono" | "multi"
 
   RENAME_OP_MODE     = "auto",
-  WRITE_MEDIA_CUES   = 1,
-  WRITE_TAKE_MARKERS = 1,
+  WRITE_EDGE_CUES    = 1,
+  WRITE_GLUE_CUES    = 1,
 }
 
 local function set(k, v) r.SetProjExtState(0, NS, k, tostring(v)) end
@@ -45,11 +45,11 @@ r.ShowConsoleMsg(string.format(
   "EPSILON_MODE=%s\nEPSILON_VALUE=%s\nDEBUG_LEVEL=%s\n"..
   "GLUE_TAKE_FX=%s\nGLUE_TRACK_FX=%s\nGLUE_APPLY_MODE=%s\n"..
   "RENDER_TAKE_FX=%s\nRENDER_TRACK_FX=%s\nRENDER_APPLY_MODE=%s\n"..
-  "RENAME_OP_MODE=%s\nWRITE_MEDIA_CUES=%s\nWRITE_TAKE_MARKERS=%s\n",
+  "RENAME_OP_MODE=%s\nWRITE_EDGE_CUES=%s\nWRITE_GLUE_CUES=%s\n",
   NS,
   CFG.GLUE_SINGLE_ITEMS, CFG.HANDLE_MODE, CFG.HANDLE_SECONDS,
   CFG.EPSILON_MODE, CFG.EPSILON_VALUE, CFG.DEBUG_LEVEL,
   CFG.GLUE_TAKE_FX, CFG.GLUE_TRACK_FX, CFG.GLUE_APPLY_MODE,
   CFG.RENDER_TAKE_FX, CFG.RENDER_TRACK_FX, CFG.RENDER_APPLY_MODE,
-  CFG.RENAME_OP_MODE, CFG.WRITE_MEDIA_CUES, CFG.WRITE_TAKE_MARKERS
+  CFG.RENAME_OP_MODE, CFG.WRITE_EDGE_CUES, CFG.WRITE_GLUE_CUES
 ))
